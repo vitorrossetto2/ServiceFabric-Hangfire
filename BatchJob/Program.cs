cc7231a48c627ee751c8d58c6d2171652571d4bd
Vitor Rossetto <vitor.rossetto@verity.com.br>
2018-06-01 16:51:25 -0300

    Add project files.

---
+++ BatchJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BatchJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
